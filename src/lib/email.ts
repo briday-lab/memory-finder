2904f32a78e8b7493d7b1b0571540394558c1c50
Richard Mugwaneza <richardmugwaneza@Richards-MacBook-Pro.local>
2025-09-24 19:52:22 -0400

    Fix nodemailer createTransport typo

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -18,7 +18,7 @@ const createTransporter = () => {
     return null
   }
 
-  return nodemailer.createTransporter(EMAIL_CONFIG)
+  return nodemailer.createTransport(EMAIL_CONFIG)
 }
 
 export interface ProjectInvitationData {
